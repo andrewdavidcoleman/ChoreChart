afc452122fb141ab1cdf0b1bdf00d4ca0b17dc9c
Andrew Coleman <andrewdavidcoleman@gmail.com>
2021-04-19 16:44:30 -0400

    Updates

---
+++ precache-manifest.487df86ffd26e52bd5715ed499420214.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75ce3a283705ab3e808bb322d811b182",
+    "url": "/ChoreChart/index.html"
+  },
+  {
+    "revision": "9c88d4c26fad49c21634",
+    "url": "/ChoreChart/static/css/main.52d0d056.chunk.css"
+  },
+  {
+    "revision": "d7076371eb04d6b5ab21",
+    "url": "/ChoreChart/static/js/2.c89ba605.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/ChoreChart/static/js/2.c89ba605.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c88d4c26fad49c21634",
+    "url": "/ChoreChart/static/js/main.eadcff33.chunk.js"
+  },
+  {
+    "revision": "ca0aad8cacc4ffc02c17",
+    "url": "/ChoreChart/static/js/runtime-main.4452397b.js"
+  },
+  {
+    "revision": "0bb428459c8ecfa61b22a03def1706e6",
+    "url": "/ChoreChart/static/media/fa-regular-400.0bb42845.svg"
+  },
+  {
+    "revision": "1f77739ca9ff2188b539c36f30ffa2be",
+    "url": "/ChoreChart/static/media/fa-regular-400.1f77739c.ttf"
+  },
+  {
+    "revision": "7124eb50fc8227c78269f2d995637ff5",
+    "url": "/ChoreChart/static/media/fa-regular-400.7124eb50.woff"
+  },
+  {
+    "revision": "7630483dd4b0c48639d2ac54a894b450",
+    "url": "/ChoreChart/static/media/fa-regular-400.7630483d.eot"
+  },
+  {
+    "revision": "f0f8230116992e521526097a28f54066",
+    "url": "/ChoreChart/static/media/fa-regular-400.f0f82301.woff2"
+  },
+  {
+    "revision": "1042e8ca1ce821518a2d3e7055410839",
+    "url": "/ChoreChart/static/media/fa-solid-900.1042e8ca.eot"
+  },
+  {
+    "revision": "376c1f97f6553dea1ca9b3f9081889bd",
+    "url": "/ChoreChart/static/media/fa-solid-900.376c1f97.svg"
+  },
+  {
+    "revision": "605ed7926cf39a2ad5ec2d1f9d391d3d",
+    "url": "/ChoreChart/static/media/fa-solid-900.605ed792.ttf"
+  },
+  {
+    "revision": "9fe5a17c8ab036d20e6c5ba3fd2ac511",
+    "url": "/ChoreChart/static/media/fa-solid-900.9fe5a17c.woff"
+  },
+  {
+    "revision": "e8a427e15cc502bef99cfd722b37ea98",
+    "url": "/ChoreChart/static/media/fa-solid-900.e8a427e1.woff2"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ef24695d510371cd5cf06a1c633ec81c.js"
+  "/ChoreChart/precache-manifest.487df86ffd26e52bd5715ed499420214.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ChoreChart/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
